4812127d1d944a125071501cd0e3ab6cc239942e
기린긔린 <@121.169.203.45>
2019-12-08 20:11:32 +0900

    긴 문장을 클라이언트로부터 받고 그것을 반환해주는 방법

---
+++ JSPPrj/src/com/sjna0730/web/NoticeReg.java
@@ -0,0 +1,27 @@
+package com.sjna0730.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/notice-reg") //어노테이션으로 URL 매핑을 해준다. reg.html에서 보내는 post 를 받는 역할을 한다.
+public class NoticeReg extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String title = request.getParameter("title"); //reg.html에 title에 해당하는것을 받아온다.
+		String content = request.getParameter("content");
+		
+		out.println(title); //받아온 title을 출력해준다 밑의 content도 마찬가지
+		out.println(content);
+	}
+}
