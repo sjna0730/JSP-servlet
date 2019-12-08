cb32a8ff92edc9b226b21181615fd618066fbf5f
기린긔린 <@121.169.203.45>
2019-12-08 19:13:27 +0900

    JSP/servlet response와 한글인코딩,content 정하기

---
+++ JSPPrj/src/com/sjna0730/web/Nana.java
@@ -0,0 +1,25 @@
+package com.sjna0730.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hi") //어노테이션으로 URL 매핑을 해준다. 이로써 web.xml에 survlet 매핑 선언을 안해도 된다..
+public class Nana extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		for(int i=0; i<100; i++)
+			out.println((i+1)+": 안녕 ~servlet<br>");
+		
+	}
+}
